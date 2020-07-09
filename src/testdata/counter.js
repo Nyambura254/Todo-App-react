a2e160440d65819432ab410e81ffb1485bd156b6
BonnieNyambura <nyamburabonnie@gmail.com>
2020-07-09 15:02:31 +0300

    react todo app

--- src/testdata/counter.js
+++ src/testdata/counter.js
@@ -1,64 +1,46 @@
 class Counter extends React.Component {
   constructor(props) {
     super(props);
-     this.increment = this.increment.bind(this)
-     this.decrement = this.decrement.bind(this)
-     this.reset = this.reset.bind(this)
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
+    this.reset = this.reset.bind(this)
     this.state = {
       count: 0
     };
 
-  //increment function
+    //increment function
 
-  increment() {
-    this.setState({
-      if (!this.state===0){
-       return this.state.const();
-      };else{
-
-      return this.state.count: this.state.count + 1
-    }
+    increment() {
+      this.setState({
+        this.state.count: this.state.count + 1
+      });
     };
-  });
-  //decrement function
-  decrement() {
-    this.setState({
-      if(this.state.length>0){
-        return this.state.count: this.state.count - 1;
-      }else{
-        if(this.state.length<=0){
-          console.log('Nothing to decrement')
-        }
-      }
-      
-    });
+
+    decrement() {
+      this.setState({
+        this.state.count: this.state.count - 1
+      });
+    };
+
+    //reset function
+
+    reset() {
+      this.setState({
+        this.state.count: 0;// back at zero
+      });
+    };
+
+
+    render() {
+      return (
+
+        <div>
+          <button></button>
+          <button></button>
+          <button></button>
+          <h1>Display Count: {this.state.count}</h1>
+        </div>
+      );
+    }
+
   };
-
-  //reset function
-
-  reset() {
-    this.setState({
-      this.state.count: 0;// back at zero
-    });
-  };
-
- 
-  render() {
-    return (
-
-   <div>
-   <button></button>
-    <button></button>
-    <button></button>
-    <h1>Display Count: {this.state.count}</h1>
-  </div>
-    );
-  }
-
-};
-/// const jsx = (
-//     <div>
-//         <Counter/>
-//     </div>
-// );
-// ReactDOM.render(<TodoApp />, document.getElementById("app"));
