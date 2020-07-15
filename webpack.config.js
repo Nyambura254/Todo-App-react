c089f20533eb35868264ce16de4b6c550941d96d
BonnieNyambura <nyamburabonnie@gmail.com>
2020-07-15 17:58:42 +0300

    react todo task app

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,46 @@
-var path = require('path');
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, "public"),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [{
-                loader: "babel-loader",
-                test: /\.js$/,
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.s?css$/, //run either css or scss
-                use: ["style-loader", "css-loader", "sass-loader"],
-            },
-        ],
-    },
-    devtool: "cheap-module-eval-source-map",
-    devServer: {
-        contentBase: path.join(__dirname, "public"),
-    },
+var path = require("path");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = (env) => {
+    var isProduction = env === "production";
+    var CSSExtract = new ExtractTextPlugin("todoapp.css");
+
+    return {
+        entry: "./src/app.js",
+        output: {
+            path: path.join(__dirname, "public"),
+            filename: "bundle.js",
+        },
+        module: {
+            rules: [{
+                    loader: "babel-loader",
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.s?css$/,
+                    use: CSSExtract.extract({
+                        use: [{
+                                loader: "css-loader",
+                                options: {
+                                    sourceMap: true,
+                                },
+                            },
+                            {
+                                loader: "sass-loader",
+                                options: {
+                                    sourceMap: true,
+                                },
+                            },
+                        ],
+                    }),
+                },
+            ],
+        },
+        plugins: [CSSExtract],
+        devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
+        devServer: {
+            contentBase: path.join(__dirname, "public"),
+        },
+    };
 };
